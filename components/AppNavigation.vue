289eefc28fa8a9f3192ddf77a05babc815922cc6
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-11-24 22:00:29 +0100

    Add a mobile navigation

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -1,5 +1,26 @@
+<script setup>
+	const entries = [
+		{
+			name: 'Home',
+			uri: '/',
+			icon: 'heroicons:home-solid',
+		},
+		{
+			name: 'Games',
+			uri: '/games',
+			icon: 'heroicons:puzzle-piece-solid',
+		},
+	];
+</script>
+
 <template>
-	<nav>
-		Des ist die navigation
+	<nav class="absolute bg-gradient-to-t from-orange-50 via-orange-50 to-white bottom-0 w-full py-2">
+		<ul class="flex justify-evenly flex-row">
+			<li v-for="entry of entries" class="flex items-center gap-1">
+				<a :href="entry.uri" :title="entry.name" class="aspect-square p-1 rounded-sm hover:bg-gray-100 focus:bg-gray-100 transition-colors">
+					<Icon :name="entry.icon" size="1.5rem" />
+				</a>
+			</li>
+		</ul>
 	</nav>
 </template>
