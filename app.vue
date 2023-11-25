04c31e83f9114858a9f966a9d10b5f18c80336e8
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-11-25 11:34:15 +0100

    Design the navigation for desktop usage

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="flex">
-    <AppNavigation />
-    <main>
+  <div class="flex min-h-screen">
+    <div class="w-0 sm:w-1/3 flex gap-0.5">
+      <AppNavigation />
+      <div class="bg-gray-200 w-0.5" />
+    </div>
+    <main class="px-2 sm:w-2/3">
       <NuxtPage />
     </main>
   </div>
--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -15,7 +15,7 @@ const entries = [
 export default {
   data() {
     return {
-      entries: entries,
+      entries,
     };
   },
 };
@@ -23,16 +23,23 @@ export default {
 
 <template>
   <nav
-    class="absolute bg-gradient-to-t from-orange-50 via-orange-50 to-white bottom-0 w-full py-2"
+    class="absolute sm:static sm:bg-gradient-to-r sm:bottom-auto bg-gradient-to-t from-orange-50 via-orange-50 sm:via-white to-white bottom-0 w-full py-2 sm:h-full"
   >
-    <ul class="flex justify-evenly flex-row">
-      <li v-for="entry of entries" class="flex items-center gap-1">
+    <ul class="flex justify-evenly flex-row sm:gap-1 sm:flex-col">
+      <li
+        v-for="entry of entries"
+        :key="entry.name"
+        class="flex items-center gap-1 sm:px-2"
+      >
         <a
           :href="entry.uri"
           :title="entry.name"
-          class="aspect-square p-1 rounded-sm hover:bg-gray-100 focus:bg-gray-100 transition-colors"
+          class="aspect-square p-1 rounded-sm hover:bg-gray-100 focus:bg-gray-100 transition-colors flex items-center gap-1 sm:aspect-auto sm:w-full"
         >
           <Icon :name="entry.icon" size="1.5rem" />
+          <span class="hidden sm:inline-block">
+            {{ entry.name }}
+          </span>
         </a>
       </li>
     </ul>
