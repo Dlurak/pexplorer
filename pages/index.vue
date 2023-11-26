9f3ccf2ed52171ceebdc082002ca88a252c49073
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-11-26 12:41:09 +0100

    Semantic and Optical improvments

--- pages/index.vue
+++ pages/index.vue
@@ -28,6 +28,16 @@ const { data, error, fetchNextPage, isFetchingNextPage, hasNextPage } =
     getNextPageParam: (_, __, currentPage) => currentPage + 1,
     initialPageParam: 0,
   });
+
+const flat = () => {
+  const pages = data?.value?.pages || [];
+  let results: PokemonBase[] = [];
+  pages.forEach((page) => {
+    const pageResults = page?.results || [];
+    results = [...results, ...pageResults];
+  });
+  return results;
+};
 </script>
 
 <template>
@@ -35,21 +45,17 @@ const { data, error, fetchNextPage, isFetchingNextPage, hasNextPage } =
     <div v-if="error">Something went wrong :/</div>
     <div v-if="data && data.pages" class="gap-3 flex flex-col">
       <ul
-        v-for="page of data.pages"
-        :key="page?.count"
         class="grid grid-cols-[repeat(auto-fit,minmax(min(20rem,100%),1fr))] gap-3"
       >
-        <li v-for="result of page?.results || []" :key="result.name">
+        <li v-for="result of flat()" :key="result.name">
           <PokemonCard :name="result.name" :url="result.url" />
         </li>
-      </ul>
-      <ul
-		  v-if="isFetchingNextPage"
-        class="grid grid-cols-[repeat(auto-fit,minmax(min(20rem,100%),1fr))] gap-3"
-      >
-        <li v-for="_ of new Array(15).fill(null)">
-          <SkeletonCard />
-        </li>
+
+        <div v-if="isFetchingNextPage" class="contents">
+          <li v-for="_ of new Array(15).fill(null)">
+            <SkeletonCard />
+          </li>
+        </div>
       </ul>
       <button
         :disabled="isFetchingNextPage || !hasNextPage"
