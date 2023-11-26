dd56dcc7cdfeeed6ea1524baeeb3ad81d6bdc72e
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-11-26 10:58:45 +0100

    Implement infinite scrolling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/image"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxt/image",
+    "@hebilicious/vue-query-nuxt",
+  ],
   devtools: { enabled: true },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { useInfiniteQuery } from "@tanstack/vue-query";
 type PokemonBase = {
   name: string;
   url: string;
@@ -11,21 +12,43 @@ type PokeResponse = {
   results: PokemonBase[];
 };
 
-const response = await useFetch<PokeResponse>(
-  "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=11",
-);
-const data = response.data.value;
+const query = async ({ pageParam = 0 }) => {
+  const perPage = 12;
+  const offset = pageParam * perPage;
+  const response = await useFetch<PokeResponse>(
+    `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${perPage}`,
+  );
+  return response.data.value;
+};
+
+const { data, error, fetchNextPage, isFetchingNextPage, hasNextPage } =
+  useInfiniteQuery({
+    queryKey: ["pokemonList"],
+    queryFn: query,
+    getNextPageParam: (_, __, currentPage) => currentPage + 1,
+    initialPageParam: 0,
+  });
 </script>
 
 <template>
   <div>
-    <ul
-      v-if="data"
-      class="grid grid-cols-[repeat(auto-fit,minmax(min(20rem,100%),1fr))] gap-3"
-    >
-      <li v-for="result of data.results" :key="result.name">
-        <PokemonCard :name="result.name" :url="result.url" />
-      </li>
-    </ul>
+    <div v-if="error">Something went wrong :/</div>
+    <div v-if="data && data.pages" class="gap-3 flex flex-col">
+      <ul
+        v-for="page of data.pages"
+        :key="page?.count"
+        class="grid grid-cols-[repeat(auto-fit,minmax(min(20rem,100%),1fr))] gap-3"
+      >
+        <li v-for="result of page?.results || []" :key="result.name">
+          <PokemonCard :name="result.name" :url="result.url" />
+        </li>
+      </ul>
+      <button
+        :disabled="isFetchingNextPage || !hasNextPage"
+        @click="() => fetchNextPage()"
+      >
+        Fetch next page
+      </button>
+    </div>
   </div>
 </template>
